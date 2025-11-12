8c908e849febf657831627e9bf97704a010fe3a6
jimmyjoseph-web3 <jimmyjoseph3659@gmail.com>
2025-11-12 14:35:16 +0800

    added register user route

---
+++ src/app/api/registerUser/route.ts
@@ -0,0 +1,37 @@
+import { NextResponse } from "next/server";
+import { db } from "../../lib/firebaseAdmin";
+export async function POST(request: Request) {
+  try {
+    const { walletAddress } = await request.json();
+
+    if (!walletAddress) {
+      return NextResponse.json(
+        { error: "Missing walletAddress" },
+        { status: 400 }
+      );
+    }
+
+    const userRef = db.ref(`users/${walletAddress}`);
+
+    // Create the new user entry
+    await userRef.set({
+      isInvestor: false,
+      totalChargeKWh: 0,
+      offsetCo2: 0,
+      totalInvestIota: 0,
+      totalIotaEarnings: 0,
+      boltCreditAmount: 0
+    });
+
+    return NextResponse.json(
+      { message: "User registered successfully" },
+      { status: 201 }
+    );
+  } catch (error) {
+    console.error("Error registering user:", error);
+    return NextResponse.json(
+      { error: "Failed to register user" },
+      { status: 500 }
+    );
+  }
+}
